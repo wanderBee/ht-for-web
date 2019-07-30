222237858abae885dec5655eda8c5d5ab6fb51e3
wangpf <wander2th@gmail.com>
2019-07-30 15:58:32 +0800

    feat: 修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,19 @@ export default {
       objs: {},
       g3d: {},
       booming: false,
+      defaultColors: ["#2D8EFF", "#36C626", "#F5222D"],
+      defaultMats: ["铝合金", "钢制品", "铁制品"],
+      defaultSizes: ["300 x 200 x 100", "150 x 250 x 70", "180 x 270 x 250"],
+      defaultUrls: [
+        "http://www.cnlaunch.com",
+        "http://www.cnlaunch.com",
+        "http://www.cnlaunch.com"
+      ],
+      defaultImages: [
+        "/image/pix02.jpg",
+        "/image/pix03.jpg",
+        "/image/pix01.jpg"
+      ],
       isAnimating: false
     };
   },
@@ -25,92 +38,81 @@ export default {
       this.initDataModel();
       // this.initTask();
     },
-    showgem(type) {
+    createNode(dataModel, type) {
       var node = new ht.Node();
+      let currentColor = this.defaultColors[Object.keys(this.objs).length];
+      let currentMat = this.defaultMats[Object.keys(this.objs).length];
+      let currentSize = this.defaultSizes[Object.keys(this.objs).length];
+      let currentUrl = this.defaultUrls[Object.keys(this.objs).length];
+      let currentImage = this.defaultImages[Object.keys(this.objs).length];
       node.s({
         shape3d: type,
+        "shape3d.color": currentColor,
         "wf.visible": "selected",
-        "3d.selectable": false
+        "3d.selectable": true,
+        "3d.movable": false
+      });
+      node.a({
+        mat: currentMat,
+        size: currentSize,
+        url: currentUrl,
+        image: currentImage
       });
 
-      this.dataModel.add(node);
+      dataModel.add(node);
       this.objs[type] = node;
     },
-    initDataModel() {
-      let dataModel = new ht.DataModel();
-      let g3d = new ht.graph3d.Graph3dView(dataModel);
-      g3d.setEye(-200, 350, 350);
-      g3d.setDashDisabled(false);
-      g3d.getView().style.background = "#c2c2c2";
-      g3d.addToDOM();
-      this.dataModel = dataModel;
-      this.g3d = g3d;
-
-      this.dataModel.clear();
-      this.g3d.setCenter([0, 0, 0]);
-      ht.Default.loadObj("3dobj/003.obj", null, {
-        center: false,
-        shape3d: "gem003",
-        s3: [0.04, 0.04, 0.04], // make gem smaller
-        finishFunc: (modelMap, array, rawS3) => {
-          this.showgem("gem003", rawS3);
-        }
-      });
-      ht.Default.loadObj("3dobj/001.obj", null, {
-        center: false,
-        shape3d: "gem001",
-        s3: [0.04, 0.04, 0.04], // make gem smaller
-        finishFunc: (modelMap, array, rawS3) => {
-          this.showgem("gem001", rawS3);
-        }
-      });
-      ht.Default.loadObj("3dobj/002.obj", null, {
-        center: false,
-        shape3d: "gem002",
-        s3: [0.04, 0.04, 0.04], // make gem smaller
-        finishFunc: (modelMap, array, rawS3) => {
-          this.showgem("gem002", rawS3);
-        }
-      });
-
-      let graphView = this.g3d.getView();
-      graphView.addEventListener(
-        "dblclick",
-        e => {
-          if (this.isAnimating || !ht.Default.isLeftButton(e)) {
-            return;
+    loadObj(type, url) {
+      return new Promise((resolve, reject) => {
+        ht.Default.loadObj(url, null, {
+          center: false,
+          shape3d: type,
+          s3: [0.04, 0.04, 0.04], // make gem smaller
+          finishFunc: modelMap => {
+            if (modelMap) {
+              resolve(type);
+            } else {
+              reject(null);
+            }
           }
-          e.preventDefault();
-          this.isAnimating = true;
-          console.log(">>> this.booming triggerBoom:", this.booming);
-          this.triggerBoom();
-        },
-        false
-      );
-      graphView.addEventListener(
-        "tap",
-        e => {
-          if (this.isAnimating || !ht.Default.isLeftButton(e)) {
-            return;
-          }
-          e.preventDefault();
-          this.isAnimating = true;
-          console.log(">>> this.booming triggerBoom:", this.booming);
-          this.triggerBoom();
-        },
-        false
-      );
-      graphView.addEventListener(
-        "contextmenu",
-        e => {
-          e.preventDefault();
-          alert("您点击了节点，具体节点信息暂时尚未发布。");
-        },
-        false
-      );
+        });
+      });
     },
-    triggerRightClick(event) {
-      alert("click node...");
+    initDataModel() {
+      let loadObj01 = this.loadObj("gem001", "3dobj/001.obj");
+      let loadObj02 = this.loadObj("gem002", "3dobj/002.obj");
+      let loadObj03 = this.loadObj("gem003", "3dobj/003.obj");
+      Promise.all([loadObj01, loadObj02, loadObj03]).then(results => {
+        let dataModel = new ht.DataModel();
+
+        let g3d = new ht.graph3d.Graph3dView(dataModel);
+        g3d.setEye(-200, 350, 350);
+        g3d.setDashDisabled(false);
+        g3d.getView().style.background = "#c2c2c2";
+        g3d.addToDOM();
+
+        var div = document.createElement("div");
+        div.classList.add("popper");
+        div.innerHTML = `
+            <div class="popper-head">
+              选中图元零件展现具体信息
+            </div>
+            <div class="popper-body">
+            </div>
+          `;
+        g3d.getView().appendChild(div);
+
+        results.forEach(res => {
+          if (res) {
+            this.createNode(dataModel, res);
+          }
+        });
+
+        this.dataModel = dataModel;
+        this.g3d = g3d;
+
+        this.initEvents();
+      });
     },
     triggerBoom() {
       // 第一个OBJ和第三个OBJ弹出，展示爆炸效果
@@ -139,6 +141,45 @@ export default {
         }
       });
     },
+    initEvents() {
+      let g3dView = this.g3d.getView();
+      g3dView.addEventListener(
+        "dblclick",
+        e => {
+          if (this.isAnimating || !ht.Default.isLeftButton(e)) {
+            return;
+          }
+          e.preventDefault();
+          this.isAnimating = true;
+          this.triggerBoom();
+        },
+        false
+      );
+
+      this.g3d.mi(e => {
+        console.log("e.kind", e.kind);
+        if (e.kind === "clickData") {
+          let node = e.data;
+          let displayContent = document.querySelector(".popper .popper-body");
+          if (displayContent) {
+            console.log("e.data:", node, node.s("shape3d"), node.a("mat"));
+            displayContent.innerHTML = `
+            <ul>
+              <li>名称：<span style="color:${node.s("shape3d.color")}">${node.s(
+              "shape3d"
+            )}</span></li>
+              <li>材质： ${node.a("mat")}</li>
+              <li>尺寸： ${node.a("size")}</li>
+              <li>购买链接： <a target="_blank" href="${node.a(
+                "url"
+              )}">${node.a("url")}</a></li>
+            <li><image src="${node.a("image")}"/></li>
+              </ul>
+            `;
+          }
+        }
+      });
+    },
     initTask() {
       let rotationTask = {
         interval: 16,
@@ -157,3 +198,34 @@ export default {
   }
 };
 </script>
+
+<style lang="less">
+.popper {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  width: 320px;
+  background: #fff;
+  border: 1px solid #dedede;
+  font-size: 14px;
+
+  .popper-head {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #333;
+    border-bottom: 1px solid #dedede;
+    height: 50px;
+  }
+  .popper-body {
+    padding: 16px;
+
+    ul,
+    li {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+  }
+}
+</style>
